9b3d549036ba37a00ea2925e3c69f5131b38e168
sosafe-yash-pandey <yash.pandey@sosafe.de>
2024-08-29 12:54:45 +0200

    Add mantine ui and startup ascii art

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
+
+import { Button, MantineProvider } from '@mantine/core';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch('http://localhost:3000/');
@@ -18,26 +19,11 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <MantineProvider> 
+      <Button color='#0B7285'>
+        Hello world
+      </Button>
+    </MantineProvider>
     </>
   )
 }
